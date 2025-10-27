64fb75118c8bb58ec3f3cfa5663abae94b642eb4
Torrez Lizandro Manyel <manyel.torrez@ucr.ac.cr>
2025-10-26 19:33:50 -0600

    feat: add config package initialization

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+"""
+Paquete de configuración para Maze Runner
+MO0002 - Programación I - Universidad de Costa Rica
+"""
+
+from .settings import settings, GameSettings, Paths
+
+__all__ = ['settings', 'GameSettings', 'Paths']
