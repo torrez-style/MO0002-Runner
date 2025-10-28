f2e302ee92682a6d801b8f0a3f9d686575162284
Torrez Lizandro Manyel <manyel.torrez@ucr.ac.cr>
2025-10-28 12:16:34 -0600

    Localización: Traducir nombres de funciones y variables en pathfinding.py

--- CODE_RUNNER/pathfinding.py
+++ CODE_RUNNER/pathfinding.py
@@ -17,7 +17,7 @@ def bfs_siguiente_paso(laberinto, inicio, objetivo):
         return inicio
     
     filas = len(laberinto)
-    cols = len(laberinto[0])
+    columnas = len(laberinto[0])
     
     # Cola para BFS: (posición, camino)
     cola = deque([(inicio, [inicio])])
@@ -30,25 +30,25 @@ def bfs_siguiente_paso(laberinto, inicio, objetivo):
         (x, y), camino = cola.popleft()
         
         # Explorar vecinos
-        for dx, dy in direcciones:
-            nx, ny = x + dx, y + dy
+        for delta_x, delta_y in direcciones:
+            nueva_x, nueva_y = x + delta_x, y + delta_y
             
             # Verificar límites y que sea camino
-            if 0 <= nx < cols and 0 <= ny < filas and laberinto[ny][nx] == 0:
-                if (nx, ny) not in visitados:
-                    visitados.add((nx, ny))
-                    nuevo_camino = camino + [(nx, ny)]
+            if 0 <= nueva_x < columnas and 0 <= nueva_y < filas and laberinto[nueva_y][nueva_x] == 0:
+                if (nueva_x, nueva_y) not in visitados:
+                    visitados.add((nueva_x, nueva_y))
+                    camino_nuevo = camino + [(nueva_x, nueva_y)]
                     
                     # Si llegamos al objetivo, devolver el siguiente paso
-                    if (nx, ny) == objetivo:
-                        return nuevo_camino[1] if len(nuevo_camino) > 1 else inicio
+                    if (nueva_x, nueva_y) == objetivo:
+                        return camino_nuevo[1] if len(camino_nuevo) > 1 else inicio
                     
-                    cola.append(((nx, ny), nuevo_camino))
+                    cola.append(((nueva_x, nueva_y), camino_nuevo))
     
     # No hay camino disponible
     return None
 
 
-def distancia_manhattan(pos1, pos2):
+def calcular_distancia_manhattan(posicion1, posicion2):
     """Calcula la distancia Manhattan entre dos posiciones"""
-    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+    return abs(posicion1[0] - posicion2[0]) + abs(posicion1[1] - posicion2[1])
