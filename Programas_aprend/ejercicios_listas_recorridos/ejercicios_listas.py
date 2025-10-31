e5a5e97c211fbc3f99e6f6bf5918c31f6d2e4d51
Torrez Lizandro Manyel <manyel.torrez@ucr.ac.cr>
2025-10-31 07:59:34 -0600

    aprendiendo listas

---
+++ Programas_aprend/ejercicios_listas_recorridos/ejercicios_listas.py
@@ -0,0 +1,52 @@
+"""
+Imprimir todos los elementos de una lista
+Crea una lista de 5 nombres y recórrela con un bucle for para imprimir cada nombre.
+
+
+
+lista=["Juan", "Pedro", "Anna", "Wendy", "Marcos"]
+
+for i in lista:
+    print(i)
+"""
+
+"""
+Sumar elementos numéricos
+Dada una lista de números, recorre la lista y muestra la suma total de sus elementos.
+
+"""
+numeros=[1,2,3,4,5,6]
+contador=0
+
+for i in numeros:
+    contador+=i
+    
+print(f"La suma total es: {contador}")
+
+"""
+3.Encontrar el valor máximo
+Dada una lista de enteros, recórrela y encuentra el número mayor (sin usar la función max).
+
+4.Contar elementos pares
+Dada una lista de números, recórrela y cuenta cuántos son pares.
+
+5.Buscar un elemento específico
+Recibe un elemento por input y verifica si está en una lista determinada, recorriéndola manualmente.
+
+6.Duplicar los valores de una lista
+Dada una lista de números, crea una nueva lista con el doble de cada valor usando una list comprehension o un bucle.
+
+7.Suma de los elementos en posiciones impares
+Dada una lista de números, suma sólo los que están en posiciones (índices) impares.
+
+8.Imprimir los elementos en orden inverso
+Dada una lista, recórrela e imprime cada elemento desde el último hasta el primero.
+
+9.Eliminar todos los elementos negativos
+Dada una lista de enteros, crea una nueva con sólo los números mayores o iguales a cero.
+
+10.Concatenar dos listas elemento a elemento
+Dadas dos listas del mismo tamaño, recórralas y concatenen (como cadenas) los elementos que ocupan la misma posición. Ejemplo: [“a”, “b”] + [“x”, “y”] → [“ax”, “by”]
+
+
+"""
