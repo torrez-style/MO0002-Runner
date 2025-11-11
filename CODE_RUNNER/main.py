fad23fb860c5ecc862c717e0d3479e3ffac3e0e8
Torrez Lizandro Manyel <manyel.torrez@ucr.ac.cr>
2025-11-10 23:42:56 -0600

    Eliminar archivo main.py y su lógica de ejecución del juego

--- CODE_RUNNER/main.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from src.juego.juego import Juego
-
-if __name__ == "__main__":
-    Juego().ejecutar()
